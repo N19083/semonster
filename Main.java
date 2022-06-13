41dba8fc273b3af77d5693fc76948845d785ef4f
Masamichi Nishimura <e1n19083@st.oit.ac.jp>
2022-06-13 11:16:52 +0900

    修正

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("ローゼンクランツ");
+    System.out.println("SEMonster!");
+    Player p1 = new Player();
+    p1.drawMonsters();
+    p1.showDeck();
   }
 }
